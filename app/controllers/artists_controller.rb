0d0a3f1b339ac569bd136d33981e00d9d05af431
Joshua Tan <jtsarvida@gmail.com>
2020-09-04 07:45:52 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,7 +12,7 @@ class ArtistsController < ApplicationController
     if @preference.allow_create_artists
       @artist = Artist.new
     else
-      redirect_to artists_path 
+      redirect_to artists_path
     end
   end
 
